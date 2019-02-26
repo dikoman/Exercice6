c6028bed1b2edd94dcde2538fbd70c562e3abf36
Vincent Gagnon <dikoman123@hotmail.com>
2019-02-26 11:31:14 -0500

    ajout des dossiers Sass et Autres

---
+++ template-parts/content/content-titre-cours.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template part for displaying post archives and search results
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+?>
+
+xxxxxxxxxxxxxxx- content-titre-cours enfant -xxxxxxxxxxxxxxxxx
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php
+		
+        
+		the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		?>
+	</header><!-- .entry-header -->
+
+</article><!-- #post-${ID} -->
