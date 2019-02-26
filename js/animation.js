09d1ce6608bcb8d3603336dda1ae2a578d49e2c7
Vincent Gagnon <dikoman123@hotmail.com>
2019-02-26 14:40:09 -0500

    Animation opacity on title and description

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,13 @@
 window.addEventListener('load', function(){
 
-    console.log('Animation');
+    var element1 = document.querySelector('.site-title').style;
+
+    element1.display = "block";
+
+    var element2 = document.querySelector('.site-description').style;
+
+    element2.display = "block";
+    
+    console.log('Animation terminée!');
+
 })
